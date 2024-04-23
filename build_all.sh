1995352404f8384be8d3959c93db5ef57765f626
Steve Potter <stephen@redefinesurgery.com>
2024-04-23 14:51:44 -0400

    Update docs and stuff

--- build_all.sh
+++
@@ -1,7 +0,0 @@
-DIR=$(pwd)
-
-cd $DIR/mycpp/ && mkdir -p build && cd build && cmake .. && make -j11
-cd /kaolin && rm -rf build *egg* && pip install -e .
-cd $DIR/bundlesdf/mycuda && rm -rf build *egg* && pip install -e .
-
-cd ${DIR}
